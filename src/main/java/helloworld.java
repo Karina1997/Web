e62f7e7df9e43dcf65ff19ab569993e44c6ac950
karina <karinaanton77@gmail.com>
2017-12-01 01:44:14 +0300

    change 2

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,14 +1,16 @@
-package com.firsttry;
-import java.io.IOException;
+import java.io.*;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class helloworld {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) throws IOException {
+
+        FileReader reader = new FileReader("./src/logging.properties");
+        System.out.println(reader.read());
+
         try {
             LogManager.getLogManager().readConfiguration(
-                    helloworld.class.getResourceAsStream("/logging.properties"));
+                    helloworld.class.getResourceAsStream("./src/logging.properties"));
         } catch (IOException e) {
             e.printStackTrace();
         }
